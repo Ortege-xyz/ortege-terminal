11b384cb116c8b8f98f6b85ba2ea5745a7921077
Justin Trollip <jtrollip@protonmail.com>
2024-10-10 14:11:22 +1000

    Hardcoding JWT. Expires in 20 days so not critical

--- src/CubeProvider.tsx
+++ src/CubeProvider.tsx
@@ -3,8 +3,8 @@ import React, { createContext, useContext, ReactNode } from "react";
 import cubejs, { CubeApi } from "@cubejs-client/core";
 
 // Load environment variables
-const CUBE_API_URL = process.env.REACT_APP_CUBE_API_URL;
-const CUBE_TOKEN = process.env.REACT_APP_CUBE_TOKEN;
+const CUBE_API_URL = "https://api-staging.ortege.ai";
+const CUBE_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Img4clpoR3VHSDVRZ3MwemRpUWpBNyJ9.eyJpc3MiOiJodHRwczovL2Rldi16NGNrYWFkNzZlaWlmczNkLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJmUjBwT1NHNG5vcjlub3M5Q2hmajRhajg1MmVyOXBtREBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9hcGktc3RhZ2luZy5vcnRlZ2UuYWkiLCJpYXQiOjE3MjcyMzMzODUsImV4cCI6MTcyOTgyNTM4NSwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwiYXpwIjoiZlIwcE9TRzRub3I5bm9zOUNoZmo0YWo4NTJlcjlwbUQifQ.Wr9CWf_Phi4uUcdgEvmpqXilkb_DCW-XGXdePuMJWpgB6kB_tqvHU3phkO5qZ0AVfiCzN5yFExEbEYrTkG1OcRtIQMxsXyfXbmVhdbsHFc7WiZnyeI1SgPUpdTJPmQI78MINdioh9DgsB9mi4s42mbuCicQ59dDCcV2iDobrnVJrtTF1vt_IraDQ35dN8M4HwMZiMRW_WQoK4t-PPPPBaceBeQe3lYruG9tpEMBmayv-BAK4OypYwddcCUzqFBu4GAkN1rmMmp6ySwHqJfhQvUxMk-4HulRCWXXa9a9QWdyWm4Z5w1AKU9JoGyfKfwzMUrFBFlZFefG1EhyiX9-Y2A";
 
 if (!CUBE_API_URL || !CUBE_TOKEN) {
   throw new Error("CUBE_API_URL and CUBE_TOKEN must be defined in environment variables");
